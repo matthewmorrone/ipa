ee88f31ff0f34006e3bcf49ddcb0346f3be5de06
matthewmorrone <matthewmorrone@francis-lewis.dreamhost.com>
2017-04-17 09:28:14 -0700

    much needed update, prettification, clean-up, bug fixing

--- ipa.php
+++ ipa.php
@@ -16,7 +16,7 @@ function echoecho() {
 	}
 }
 function xerror($query) {
-	print("<br /><br />Error At:<br />" . $query . "<br />" . mysql_error() . "<br />");
+	print("<br /><br />Error At:<br />".$query."<br />".mysql_error()."<br />");
 }
 function chomp($query, $c) {
 	return substr($query, 0, strlen($query) - $c);
@@ -30,8 +30,8 @@ function load_sounds() {
 		if ($dh = opendir($dir)) {
 			while (($file = readdir($dh)) !== false) {
 				if (strpos($file, ".mp3")) {
-					print('<button id="button' . $n . '" class="play" title="' . $n . '" value="' . $file . '"></button>');
-					print('<audio id="sounds/' . $file . '" src="sounds/' . $file . '" preload=none></audio>');
+					print('<button id="button'.$n.'" class="play" title="'.$n.'" value="'.$file.'"></button>');
+					print('<audio id="sounds/'.$file.'" src="sounds/'.$file.'" preload=none></audio>');
 					$n++;
 				}
 			}
@@ -51,21 +51,27 @@ function languages($dir) {
 				if ($file == "IPA.txt") {
 					echo ("selected");
 				}
-				echo (">" . ucfirst(chomp($file, 4)) . "</option>");
+				echo (">".ucfirst(chomp($file, 4))."</option>");
 			}
 			echo ("</select>");
 		}
 	}
 }
-function search($lookup, $letters) {foreach ($letters as $l) {if ($l[0] == $lookup) {return 1;}}}
+function search($lookup, $letters) {
+	foreach ($letters as $l) {
+		if ($l[0] == $lookup) {
+			return 1;
+		}
+	}
+}
 function print_chart($reference) {
 	$filename = $_POST["filename"];
 	$name = ucfirst(chomp($filename, 4));
-	$letters = file("languages/" . $filename);
+	$letters = file("languages/".$filename);
 	foreach ($letters as &$letter) {
 		$letter = explode(", ", $letter);
 	}
-	$lookup = file("tables/" . $reference . ".txt");
+	$lookup = file("tables/".$reference.".txt");
 	for ($x = 0; $x < count($lookup); $x++) {
 		$lookup[$x] = explode(',', $lookup[$x]);
 	}
@@ -75,16 +81,15 @@ function print_chart($reference) {
 	$k = 1;
 
 	echo "<div class='half'>";
-	print('<table id="' . $reference . '">');
+	print('<table id="'.$reference.'">');
 	print('<tr>');
 	$k += 2;
 	print('<th></th>');
-	for ($i = 0; $i < ($x / 2); $i++) //&nbsp;
-	{
+	for ($i = 0; $i < ($x / 2); $i++) {
 		$caption = str_replace("http://en.wikipedia.org/wiki/", "", $lookup[$k][2]);
 		$caption = str_replace("_", " ", $caption);
 		$caption = ucwords($caption);
-		print('<th colspan="2" class="rotate" id="' . $lookup[$k][1] . '"><input type="checkbox" checked="checked" style="display: none;"><div><a href="' . $lookup[$k][2] . '" title="' . $caption . '">' . $lookup[$k][1] . '</a></div></input></th>');
+		print('<th colspan="2" class="rotate" id="'.$lookup[$k][1].'"><input type="checkbox" checked="checked" style="display: none;"><div><a target="_blank" href="'.$lookup[$k][2].'" title="'.$caption.'">'.$lookup[$k][1].'</a></div></input></th>');
 		$k++;
 	}
 	print('</tr>');
@@ -94,7 +99,7 @@ function print_chart($reference) {
 		$caption = str_replace("_", " ", $caption);
 		$caption = ucwords($caption);
 		$lookup[$k][3] = chomp($lookup[$k][3], 1);
-		print("<th class='side' id='" . $lookup[$k][1] . "'><input type='checkbox' checked='checked' style='display: none;'><div><a href='" . $lookup[$k][2] . "' title='" . $caption . "'>" . $lookup[$k][1] . "</a></div></input></th>");
+		print("<th class='side' id='".$lookup[$k][1]."'><input type='checkbox' checked='checked' style='display: none;'><div><a target='_blank' href='".$lookup[$k][2]."' title='".$caption."'>".$lookup[$k][1]."</a></div></input></th>");
 		$k++;
 		for ($i = 1; $i < $x + 1; $i++) {
 			if ($lookup[$k][1] != '' && (search($lookup[$k][1], $letters) || $filename == "All")) {
@@ -102,12 +107,12 @@ function print_chart($reference) {
 				$caption = str_replace("_", " ", $caption);
 				$caption = ucwords($caption);
 				if ($lookup[$k][4] != '') {
-					$caption .= " (" . $lookup[$k][4] . ")";
+					$caption .= " (".$lookup[$k][4].")";
 				}
 				$lookup[$k][5] = substr($lookup[$k][5], 0, strlen($lookup[$k][5]) - 1);
-				print("<td number='" . $lookup[$k][4] . "' title='" . $caption . "' id='" . $lookup[$k][1] . "'><a href='" . $lookup[$k][2] . "' title='" . $caption . "' class='sound' target='_blank'>" . $lookup[$k][1] . "</a>"); // onmouseover='display(".$lookup[$k][1].")' onmouseout='display(\"\")' class=sound ondblclick='ipaappend(".$lookup[$k][1].")'
+				print("<td number='".$lookup[$k][4]."' title='".$caption."' id='".$lookup[$k][1]."'><a href='".$lookup[$k][2]."' title='".$caption."' class='sound' target='_blank'>".$lookup[$k][1]."</a>"); // onmouseover='display(".$lookup[$k][1].")' onmouseout='display(\"\")' class=sound ondblclick='ipaappend(".$lookup[$k][1].")'
 				if ($lookup[$k][3] != "") {
-					print('<audio id="' . $lookup[$k][3] . '" src="' . $lookup[$k][3] . '" preload=none></audio>');
+					print('<audio id="'.$lookup[$k][3].'" src="'.$lookup[$k][3].'" preload=none></audio>');
 				}
 				print("</td>");
 			} else {
@@ -118,23 +123,26 @@ function print_chart($reference) {
 		print('</tr>');
 	}
 	print("</table>");
-	print("<table id='" . $lookup[1][0] . "' class=sub><tr><th class='third'><input type='checkbox' checked='checked' style='display: none;'><a title='" . $lookup[1][1] . "'>" . $lookup[1][1] . "</a></input></th><th class='third'><input type='checkbox' checked='checked' style='display: none;'><a title='" . $lookup[1][2] . "'>" . $lookup[1][2] . "</a></input></th></tr></table>");
+	print("<table id='".$lookup[1][0]."' class=sub><tr><th class='third'><input type='checkbox' checked='checked' style='display: none;'><a title='".$lookup[1][1]."'>".$lookup[1][1]."</a></input></th><th class='third'><input type='checkbox' checked='checked' style='display: none;'><a title='".$lookup[1][2]."'>".$lookup[1][2]."</a></input></th></tr></table>");
 	echo "</div>";
 }
 function other_tables($name) {
-	$file = file("tables/" . strtolower($name) . ".txt");
+	$file = file("tables/".strtolower($name).".txt");
 	$result = "<table>";
-	$result .= "<tr><th style='text-align: center;' colspan='" . count(explode(",", $file[0])) . "'>$name</th></tr>";
+	$result .= "<tr><th style='text-align: center;' colspan='".count(explode(",", $file[0]))."'>$name</th></tr>";
 	foreach ($file as $line) {
 		$result .= "<tr>";
 		$line = explode(",", $line);
 		foreach ($line as $key => $part) {
-
+			// if ($key == 0) {
+			// 	$result .= "<th class=\"clickah\">".$part."</th>";
+			// }
+			// else
 			if ($key % 2 == 0) {
-				$result .= "<td class=\"clickah\">" . $part . "</td>";
-
-			} else {
-				$result .= "<td>" . $part . "</td>";
+				$result .= "<td class=\"clickah\">".$part."</td>";
+			}
+			else {
+				$result .= "<td>".$part."</td>";
 			}
 		}
 		$result .= "</tr>";
@@ -154,12 +162,12 @@ if ($mode == 2) {
 	print_chart("vowels");
 }
 if ($mode == 3) {
-	$filename = $_POST["filename"] . ".txt";
-	$file = fopen("new_languages/" . $filename, 'w');
+	$filename = $_POST["filename"].".txt";
+	$file = fopen("new_languages/".$filename, 'w');
 	$lines = $_POST["letters"];
 	print_r($lines);
 	foreach ($lines as $i) {
-		fwrite($file, $i . "\r\n");
+		fwrite($file, $i."\r\n");
 	}
 	fclose($file);
 	echo "Thank you for submitting the $filename language!";
